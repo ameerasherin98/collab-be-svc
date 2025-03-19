10eebd51aacc0f9bc7f885544280c1d498151cd2
ameera sherin <ameera@keyvalue.systems>
2025-03-19 11:34:50 +0530

    add: Define the Database Model for Brand Users

---
+++ app/models/brand_users_model.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "brand_users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    google_id = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    profile_picture = Column(String, nullable=True)
+    is_verified = Column(Boolean, default=False)
