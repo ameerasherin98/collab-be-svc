cd31a3db3cd5cf2662024dc22776480f0f31b03e
ameera sherin <ameera@keyvalue.systems>
2025-03-19 12:05:09 +0530

    update: Before inserting a user, check if they already exist. If it’s the first entry, insert a new record.

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -2,12 +2,18 @@ from sqlalchemy.orm import Session
 
 from app.models.brand_users_model import BrandUser
 
-def get_brand_user_by_google_id(db: Session, google_id: str):
-    return db.query(BrandUser).filter(BrandUser.google_id.__eq__(google_id)).first()
-
-def create_brand_user(db: Session, user_data: dict):
-    db_user = BrandUser(**user_data)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+def get_or_create_brand_user(db: Session, google_id: str, name: str, email: str, profile_picture: str):
+    user = db.query(BrandUser).filter(BrandUser.google_id == google_id).first()
+    if not user:
+        # First-time user, create a new entry
+        user = BrandUser(
+            google_id=google_id,
+            name=name,
+            email=email,
+            profile_picture=profile_picture,
+            is_verified=True
+        )
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+    return user
